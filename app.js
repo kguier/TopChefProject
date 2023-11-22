1305da08372fd7eb87d6e6607415161db3db5484
Kira <knguier12@gmail.com>
2023-11-21 19:27:21 -0500

    Created Search Cuisines Function

--- app.js
+++ app.js
@@ -158,7 +158,16 @@ function findItalianFood(allDishes) {
 function searchCuisines(allDishes) {
     alert("Searching for dishes by cuisine...")
     // TODO #3: Gather user input for a cuisine to search for, then filter for all dishes matching this cuisine type
+    let userInput = prompt("Please enter the type of cuisine you would like to see: Italian, Mexican, French, Irish, Vegetarian, or Hungarian");
+    let results = allDishes.filter(function (el) {
+        if (el.cuisine === userInput) {
+            return true;
+        } else {
+            return false;
+        }
+    });
     alert("Found all dishes matching the cuisine search term!  Check the console for full output")
+    return results
 }
 
 function searchIngredients(allDishes) {
