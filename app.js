e6c441e21e4935dd7fd5581aed87dc7538836f60
Kira <knguier12@gmail.com>
2023-11-28 13:58:51 -0500

    Completed generateCuisineDishName function

--- app.js
+++ app.js
@@ -188,7 +188,11 @@ function searchIngredients(allDishes) {
 function generateCuisineDishName(allDishes) {
     alert("Combining cuisine and dish names...")
     // TODO #5: Apply the concatenatorFunction to each dish in allDishes, then log to the console the modified result
+    let cuisineDishName = allDishes.map(function (el) {
+        return el.cuisine + " " + el.name
+    })
     alert("Successfully combined cuisine and dish names!  Check the console for full output.")
+    console.log(cuisineDishName)
 }
 
 // <<<<<<<<<<<<<<<<< EMAIL AND TEXT MARKETING MESSAGES <<<<<<<<<<<<<<<<<
@@ -201,7 +205,7 @@ function emailMessage(dishOfTheDay) {
     Thank you for subscribing to email alert messages!
     Today's Dish of the day is:
 
-    <DISH OF THE DAY HERE>
+    ${todaysSpecialDish}
 
     We hope to see you in soon!
 
